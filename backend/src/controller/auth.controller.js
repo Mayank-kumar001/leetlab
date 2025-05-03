cf1745596a06e2156b9429ea7ee2a785457e0338
Mayank-kumar001 <dev.01mayank@gmail.com>
2025-05-03 16:51:13 +0530

    add login user functionality

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -99,8 +99,53 @@ export const registerUser = async (req, res) => {
 
 
 };
-export const loginUser = (req, res) => {
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if(!email || !password){
+            throw new apiError(400, "Email and password are required");
+        }
+        const user = await db.user.findUnique({
+            where:{email},
+        })
+        if(!user){
+            throw new apiError(401, "User not found");
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
     
+        if(!isPasswordCorrect){
+            throw new apiError(401, "Invalid Credentials");
+        }
+    
+        const token = await jwt.sign({id:user.id}, process.env.JWT_SECRET, { expiresIn: "7d" })
+    
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV!== "development",
+            maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+        })
+        res.status(201).json(new apiResponse(201,{
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            role: user.role,
+        }, "User login succesfull"))
+    } catch (error) {
+        if(error instanceof apiError){
+            return res.status(error.statusCode).json({
+                statusCode: error.statusCode,
+                message: error.message,
+                success: false,
+            })
+        }
+        return res.status(500).json({
+            statusCode: 500,
+            message: "Something went wrong while the user try to login",
+            success: false,
+        }) 
+    }
+
 };
 export const logoutUser = (req, res) => {
 
